afd90e186f9c2497abff5830150d57ad5b504781
jiangxin <janason1994@126.com>
2019-01-02 19:48:10 +0800

    路由切换抽离

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,22 @@
   <div id="app">
     <m-header></m-header>
     <m-tabs></m-tabs>
-    <transition :name="transitionName">
+    <!-- <router-swiper :router-path="routesList">
       <keep-alive>
-        <router-view
-          @touchstart.native="handleTouchStart"
-          @touchmove.native="handleTouchMove"
-          @touchend.native="handleTouchEnd"
-          class="transitions"
-        ></router-view>
+        <router-view></router-view>
       </keep-alive>
-    </transition>
+    </router-swiper>-->
+    <router-view></router-view>
   </div>
 </template>
 <script>
 import MHeader from 'base/header/mheader'
 import MTabs from 'views/tabs/tabs'
-const MAX_DIFF = 50
+import RouterSwiper from 'base/router-swiper/router-swiper'
+
 export default {
   data() {
     return {
-      transitionName: 'transitionLeft',
-      currentPath: '',
-      currentIndex: 0,
-      touches: {},
       routesList: [
         {
           path: '/'
@@ -41,66 +34,13 @@ export default {
       ]
     }
   },
-  created() {
-    this.currentPath = this.$route.path
-  },
-  methods: {
-    handleTouchStart(e) {
-      const touches = e.touches[0]
-      this.touches.startX = touches.pageX
-      this.touches.startY = touches.pageY
-    },
-    handleTouchMove(e) {
-      const touches = e.touches[0]
-      this.touches.endX = touches.pageX
-      this.touches.endY = touches.pageY
-    },
-    handleTouchEnd(e) {
-      const diffX = this.touches.endX - this.touches.startX
-      const diffY = this.touches.endY - this.touches.startY
-      const diff_X_Y = Math.abs(diffX) > Math.abs(diffY)
-      const routeLen = this.routesList.length
-      this.routesList.forEach((item, index) => {
-        if (item.path === this.currentPath) {
-          this.currentIndex = index
-        }
-      })
-      if (diff_X_Y && diffX > MAX_DIFF && this.currentIndex > 0) {
-        this.transitionName = 'transitionRight'
-        this.$router.push(this.routesList[this.currentIndex - 1].path)
-      } else if (
-        diff_X_Y &&
-        diffX < -MAX_DIFF &&
-        this.currentIndex < routeLen - 1
-      ) {
-        this.transitionName = 'transitionLeft'
-        this.$router.push(this.routesList[this.currentIndex + 1].path)
-      }
-    }
-  },
-  watch: {
-    $route(to, from) {
-      const routes = this.$router.options.routes
-      const arr = []
-      routes.forEach(item => {
-        arr.push(item.path)
-      })
-      const compare = arr.indexOf(to.path) > arr.indexOf(from.path)
-      this.transitionName = compare ? 'transitionLeft' : 'transitionRight'
-      this.currentPath = to.path
-    }
-  },
+
   components: {
     MHeader,
-    MTabs
+    MTabs,
+    RouterSwiper
   }
 }
 </script>
 <style lang="stylus" scoped>
-.transitions
-  transition all 0.3s ease
-.transitionLeft-enter, .transitionRight-leave-active
-  transform translate3d(100%, 0, 0)
-.transitionRight-enter, .transitionLeft-leave-active
-  transform translate3d(-100%, 0, 0)
 </style>
---
+++ src/base/router-swiper/router-swiper.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="router-transition">
+    <transition :name="transitionName">
+      <slot></slot>
+    </transition>
+  </div>
+</template>
+
+<script>
+const MAX_DIFF = 50
+export default {
+  props: {
+    routerPath: {
+      type: Array,
+      default: []
+    }
+  },
+
+  data() {
+    return {
+      transitionName: 'transitionLeft',
+      currentPath: '',
+      currentIndex: 0,
+      touches: {}
+    }
+  },
+  mounted() {
+    this.currentPath = this.$route.path
+    this.initTouch()
+  },
+  methods: {
+    handleTouchStart(e) {
+      const touches = e.touches[0]
+      this.touches.startX = touches.pageX
+      this.touches.startY = touches.pageY
+    },
+
+    handleTouchEnd(e) {
+      e.preventDefault()
+      const touches = e.changedTouches[0]
+      this.touches.endX = touches.pageX
+      this.touches.endY = touches.pageY
+      const diffX = this.touches.endX - this.touches.startX
+      const diffY = this.touches.endY - this.touches.startY
+      const diff_X_Y = Math.abs(diffX) > Math.abs(diffY)
+      const routeLen = this.routerPath.length
+      this.routerPath.forEach((item, index) => {
+        if (item.path === this.currentPath) {
+          this.currentIndex = index
+        }
+      })
+      if (diff_X_Y && diffX > MAX_DIFF && this.currentIndex > 0) {
+        this.transitionName = 'transitionRight'
+        this.$router.push(this.routerPath[this.currentIndex - 1].path)
+      } else if (
+        diff_X_Y &&
+        diffX < -MAX_DIFF &&
+        this.currentIndex < routeLen - 1
+      ) {
+        this.transitionName = 'transitionLeft'
+        this.$router.push(this.routerPath[this.currentIndex + 1].path)
+      }
+    },
+    initTouch() {
+      this.$el.addEventListener('touchstart', this.handleTouchStart)
+      this.$el.addEventListener('touchend', this.handleTouchEnd)
+    }
+  },
+  watch: {
+    $route(to, from) {
+      const routes = this.$router.options.routes
+      const arr = []
+      routes.forEach(item => {
+        arr.push(item.path)
+      })
+      const compare = arr.indexOf(to.path) > arr.indexOf(from.path)
+      this.transitionName = compare ? 'transitionLeft' : 'transitionRight'
+      this.currentPath = to.path
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.router-transition>div
+  transition all 0.3s ease
+.transitionLeft-enter, .transitionRight-leave-active
+  transform translate3d(100%, 0, 0)
+.transitionRight-enter, .transitionLeft-leave-active
+  transform translate3d(-100%, 0, 0)
+</style>
